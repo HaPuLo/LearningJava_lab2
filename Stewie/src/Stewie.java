1580e8f6b98ec60ba7a8dd83097389ee49f1c325
Hoang <tran.ngoc.phu.hoang@intel.com>
2021-10-17 16:48:29 +0700

    Stewie

---
+++ Stewie/src/Stewie.java
@@ -0,0 +1,8 @@
+public class Stewie {
+    public static void main(String[] args){
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        //If you want to print the backslash "/" must add in one more backslash "//" before it
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
