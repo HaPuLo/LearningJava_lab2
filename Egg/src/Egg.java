1a11ce95e8e8e10ad10240f9cccec5bb13210f38
htnp <kimquanghoang94@gmail.com>
2021-10-18 08:52:47 +0700

    Egg

---
+++ Egg/src/Egg.java
@@ -0,0 +1,10 @@
+public class Egg {
+    public static void main(String[] argv){
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+}
