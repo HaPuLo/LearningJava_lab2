59d1b3c6551949b2cdec4a3ee90252dcbf058769
htnp <kimquanghoang94@gmail.com>
2021-10-18 08:45:24 +0700

    Stewie2

---
+++ Stewie2/src/Stewie2.java
@@ -0,0 +1,14 @@
+public class Stewie2 {
+    public static void main(String[] argv){
+        System.out.println("//////////////////////");
+        Stewie2Method();
+        Stewie2Method();
+        Stewie2Method();
+        Stewie2Method();
+        Stewie2Method();
+    }
+    public  static void Stewie2Method(){
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
