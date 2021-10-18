73ff0dac257d2276d86b2051585029765b91a8bc
htnp <kimquanghoang94@gmail.com>
2021-10-18 08:37:51 +0700

    Mantra

---
+++ Mantra/src/Mantra.java
@@ -0,0 +1,11 @@
+public class Mantra {
+    public static void main(String[] argv){
+        Mantra_Method();
+        System.out.println();
+        Mantra_Method();
+    }
+    public static void Mantra_Method(){
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+    }
+}
