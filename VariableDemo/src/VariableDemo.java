a6b575d8a67f088674fe58c3de3eb66fa9989aed
htnp <kimquanghoang94@gmail.com>
2021-10-20 09:54:46 +0700

    VariableDemo

---
+++ VariableDemo/src/VariableDemo.java
@@ -0,0 +1,9 @@
+public class VariableDemo {
+    public static void main(String[] argv){
+        int x= 10;
+        String MyString = "My variable x is : ";
+        System.out.println(MyString+ x);
+        x = 15;
+        System.out.println(MyString+ x);
+    }
+}
