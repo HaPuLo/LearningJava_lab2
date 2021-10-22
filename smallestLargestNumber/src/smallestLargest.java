56df8e087208ce48cc6b50c5665f8bb2e3b4ed3a
htnp <kimquanghoang94@gmail.com>
2021-10-22 16:32:01 +0700

    reup

--- smallestLargestNumber/src/smallestLargest.java
+++ smallestLargestNumber/src/smallestLargest.java
@@ -3,22 +3,26 @@ import java.io.*;
 import java.util.Arrays;
 
 public class smallestLargest {
-    public static void main(String[] argv){
+    public static void main(String[] argv) {
+        smallestLargestNumber();
+    }
+    public static void smallestLargestNumber(){
         Scanner sc = new Scanner(System.in);
         System.out.print("How many numbers do you want to enter ? ");
         int numbers_input = sc.nextInt();
-        int n = 0;
-        int smallest = 0;
-        int largest = 0;
         int[] array = new int[numbers_input];
         //Require user input the number into system
-        while (n < numbers_input){
-            System.out.print("Number " + n + " :");
+        int n = 0;
+        while (n < numbers_input) {
+            System.out.print("Number " + (n + 1) + " : ");
             int number = sc.nextInt();
             array[n] = number;
             n++;
         }
-        System.out.println(Arrays.toString(array));
+        //System.out.println(Arrays.toString(array));
+
+        int smallest = 0;
+        int largest = 0;
         smallest = largest = array[0];
         //Get the smallest and largest number
         /*
@@ -36,15 +40,14 @@ public class smallestLargest {
             if (array[m] < smallest){
                 smallest = array[m];
             }
-            else if (array[m] > largest){
+            if (array[m] > largest){
                 largest = array[m];
             }
             m++;
         }
-
         //Find the smallest and largest number
-        System.out.print("Smallest: " + smallest);
+        System.out.print("Smallest : " + smallest);
         System.out.println();
-        System.out.print("Largest: " + largest);
+        System.out.print("Largest : " + largest);
     }
 }
